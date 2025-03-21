94d7a3d9cfc262416c4f651bfdc1e7ca270f6cdc
rbandela041 <rbandela1704@gmail.com>
2025-03-21 05:28:35 -0400

    Update alb.tf

--- project3/alb.tf
+++ project3/alb.tf
@@ -1,15 +1,15 @@
 resource "aws_lb" "alb" {
-  name                       = "${local.vpc_name}-alb"
-  internal                   = false
-  load_balancer_type         = "application"
-  security_groups            = [aws_security_group.allow_all_sg.id]
-  subnets                    = aws_subnet.public-subnets[*].id
+  name               = "${local.vpc_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_all_sg.id]
+  subnets            = aws_subnet.public-subnets[*].id
   enable_deletion_protection = false
   enable_http2               = true
   idle_timeout               = 60
-}
+  }
 
-#Create ALB Listner
+  #Create ALB Listner
 resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
@@ -32,8 +32,8 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 #adding instance to target group
 resource "aws_lb_target_group_attachment" "alb_target_group_attachment" {
-  count            = 3
+  count           = 3
   target_group_arn = aws_lb_target_group.alb_target_group.arn
-  target_id        = element(aws_instance.web_server[*].id, count.index)
+  target_id        = element(aws_instance.web_server[*].id, count.index) 
   port             = 80
 }

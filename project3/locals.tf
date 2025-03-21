46718e5648a890d568e1ae45bbab9792a768b8e2
rbandela041 <rbandela1704@gmail.com>
2025-03-21 05:29:05 -0400

    Update locals.tf

--- project3/locals.tf
+++ project3/locals.tf
@@ -1,3 +1,6 @@
 locals {
-  vpc_name = lower(var.vpc_name)
+    vpc_name      = lower(var.vpc_name)
+    env           = lower(var.env)
+    dynamo_db     = lower(var.dynamo_db)
+    s3_bucket     = lower(var.s3_bucket)
 }
